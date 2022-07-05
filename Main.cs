5a2f7151907bd38e39449109e20251079ac7a254
CastAMCF <85260888+CastAMCF@users.noreply.github.com>
2022-07-05 22:12:26 +0100

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using Harmony12;
+using UnityModManagerNet;
+
+namespace MouseFix {
+	internal static class Main
+	{
+
+		public static bool enabled;
+
+		public static UnityModManager.ModEntry mod;
+
+		private static bool Load(UnityModManager.ModEntry modEntry)
+		{
+			Main.mod = modEntry;
+			modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(Main.OnToggle);
+			HarmonyInstance harmonyInstance = HarmonyInstance.Create(modEntry.Info.Id);
+			harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+			return true;
+		}
+
+		private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+		{
+			Main.enabled = value;
+			modEntry.Logger.Log("Starting MouseFix");
+			return true;
+		}
+	}
+}
---
+++ MouseMovement.cs
@@ -0,0 +1,23 @@
+﻿using Harmony12;
+using UnityEngine;
+
+namespace MouseFix {
+	[HarmonyPatch(typeof(Cursor3D))]
+	[HarmonyPatch("MouseMove")]
+	internal class MouseMovement {
+		private static bool Prefix(ref Vector2 ___screenPos, ref RectTransform ___Cursor) {
+			var gameScript = GameScript.Get();
+			if (GameMode.Get().CompareWithCurrentMode(gameMode.UI) ||
+			    gameScript != null && gameScript.CurrentSceneType == SceneType.Menu ||
+			    gameScript.CurrentSceneType == SceneType.Showroom ||
+			    gameScript.CurrentSceneType == SceneType.Auction) {
+				___screenPos.x = Input.mousePosition.x;
+				___screenPos.y = Input.mousePosition.y;
+				___Cursor.transform.position = ___screenPos;
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
